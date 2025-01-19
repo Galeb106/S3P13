f31fbc14dc8ba4754eca80213aaa48e53a068b2c
Galeb106 <gal4enkov.gleb@mail.ru>
2025-01-19 20:54:31 +0300

    Add files via upload

---
+++ Microwave.h
@@ -0,0 +1,17 @@
+#ifndef MICROWAVE_H
+#define MICROWAVE_H
+
+#include <bits/stdc++.h>
+using namespace std;
+class Microwave{
+public:
+Microwave();
+Microwave(int x,int y)
+int sttime(int x);
+int sttemp(int y);
+private:
+int time;
+int power;
+int vk;
+};
+#endif // MICROWAVE_H
